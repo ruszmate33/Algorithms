b9a7d0befcde0092853933e8b8cd978899d312f0
Mate Rusz <ruszmate33@gmail.com>
2021-03-31 12:08:22 +0200

    isOpen, isFull cleared

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -26,10 +26,10 @@ public class Percolation {
             for (int j = 1; j <= n; j++) {
                 grid[i][j] = 0;
                 if (i == 1) {
-                    wquf.union(getIDfromGrid(i, j), 0);
+                    wquf.union(getIDfromGrid(i, j), 0); // virtualTop: 0
                 }
                 if (i == n) {
-                    wquf.union(getIDfromGrid(i, j), n * n + 1);
+                    wquf.union(getIDfromGrid(i, j), n * n + 1); // virtButton
                 }
             }
         }
@@ -45,54 +45,44 @@ public class Percolation {
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (isFull(row, col)) {
+        if (!isOpen(row, col)) {
             grid[row][col] = 1;
             numOpen++;
-            // check all adjacent squares if they are open
-            int[] adjacentRows = getAdjacents(row);
-            int[] adjacentCols = getAdjacents(col);
+
             // if open connect them to newly opened site
             int newlyOpenID = getIDfromGrid(row, col);
-            for (int adjRow : adjacentRows) {
-                if (isOpen(adjRow, col)) {
-                    int adjacentID = getIDfromGrid(adjRow, col);
-                    // connection function
-                    wquf.union(newlyOpenID, adjacentID);
-                }
-            }
-            for (int adjCol : adjacentCols) {
-                if (isOpen(row, adjCol)) {
-                    int adjacentID = getIDfromGrid(row, adjCol);
-                    wquf.union(newlyOpenID, adjacentID);
-                }
-            }
+            connectIfValid(newlyOpenID, row - 1, col);
+            connectIfValid(newlyOpenID, row + 1, col);
+            connectIfValid(newlyOpenID, row, col - 1);
+            connectIfValid(newlyOpenID, row, col + 1);
         }
     }
 
-    public int[] getAdjacents(int current) {
-        int[] adjacent;
-        if (current > 1 && current < gridSize) {
-            adjacent = new int[] { current - 1, current + 1 };
+    private void connectIfValid(int newlyOpenID, int row, int col) {
+        if (isValidGrid(row, col) && isOpen(row, col)) {
+            int adjacentID = getIDfromGrid(row, col);
+            wquf.union(newlyOpenID, adjacentID);
         }
-        else if (current == 1) {
-            adjacent = new int[] { current + 1 };
-        }
-        // current == gridSize
-        else {
-            adjacent = new int[] { current - 1 };
-        }
-        return adjacent;
     }
 
+    private boolean isValidGrid(int row, int col) {
+        return (row > 0 && row <= gridSize) && (col > 0 && col <= gridSize);
+    }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         return (grid[row][col] == 1);
+        // return (grid[row][col] == 1 && !isConnected(row, col));
+    }
+
+    private boolean isConnected(int row, int col) {
+        int idx = getIDfromGrid(row, col);
+        return wquf.find(0) == wquf.find(idx);
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return (!isOpen(row, col));
+        return (grid[row][col] == 1 && !isConnected(row, col));
     }
 
     // returns the number of open sites
@@ -112,9 +102,7 @@ public class Percolation {
         while (!percolation.percolates()) {
             int randomRow = StdRandom.uniform(1, n + 1);
             int randomCol = StdRandom.uniform(1, n + 1);
-            if (percolation.isFull(randomRow, randomCol)) {
-                percolation.open(randomRow, randomCol);
-            }
+            percolation.open(randomRow, randomCol);
         }
     }
 }
